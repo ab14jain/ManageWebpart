b6ee2073660bcaee81faf0b3969b615d4367bcf8
Abhishek Kumar Jain <abhishek.kj@dgsl.in>
2020-03-05 18:43:56 +0530

    added dropdown for row and update button to log updated values for webaprt

--- src/webparts/manageWebpart/components/ManageWebpart.tsx
+++ src/webparts/manageWebpart/components/ManageWebpart.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import styles from "./ManageWebpart.module.scss";
 import { IManageWebpartProps, IWebpartDetail } from "./IManageWebpartProps";
 import { escape } from "@microsoft/sp-lodash-subset";
+import { PrimaryButton, DefaultButton } from "office-ui-fabric-react";
+
 import {
   Dropdown,
   DropdownMenuItemType,
@@ -9,6 +11,7 @@ import {
 } from "office-ui-fabric-react/lib/Dropdown";
 export interface IDropdownControlledState {
   selectedWebpart?: { key: string | number | undefined };
+  selectedZone?: { key: string | number | undefined };
   selectedSection?: { key: string | number | undefined };
   selectedOrder?: { key: string | number | undefined };
   selectedFactor?: { key: string | number | undefined };
@@ -19,13 +22,20 @@ export default class ManageWebpart extends React.Component<
 > {
   public state: IDropdownControlledState = {
     selectedWebpart: undefined,
+    selectedZone: undefined,
     selectedSection: undefined,
     selectedOrder: undefined,
-    selectedFactor: undefined,
+    selectedFactor: undefined
   };
 
   public render(): React.ReactElement<IManageWebpartProps> {
-    const { selectedWebpart,selectedSection,selectedOrder,selectedFactor } = this.state;
+    const {
+      selectedWebpart,
+      selectedZone,
+      selectedSection,
+      selectedOrder,
+      selectedFactor
+    } = this.state;
     const items = this.props.webpart.map((item, key) => (
       <div className={styles.row}>
         <div className={styles.column}>
@@ -52,15 +62,22 @@ export default class ManageWebpart extends React.Component<
     //const webpartTitle = this.props.webpart.map(wp => wp.text);
     let ddDetails: IDropdownOption[] = [];
     let sectionDetails = [];
+    let zoneDetails = [];
     let controlDetails = [];
+    let zoneDetailsDistinct = [];
     let sectionFactorDetails = [];
-    let sectionDetailsDistinct =[];
+    let sectionDetailsDistinct = [];
     let controlDetailsDistinct = [];
     let sectionFactorDetailsDistinct = [];
     this.props.webpart.forEach(wp => {
       ddDetails.push({
         key: wp.text,
         text: wp.text
+      });
+
+      zoneDetails.push({
+        key: wp.position.zoneIndex,
+        text: wp.position.zoneIndex
       });
       sectionDetails.push({
         key: wp.position.sectionIndex,
@@ -76,13 +93,14 @@ export default class ManageWebpart extends React.Component<
         key: wp.position.sectionFactor,
         text: wp.position.sectionFactor
       });
-
-
     });
-
-    sectionDetailsDistinct =  this.removeDuplicates(sectionDetails, "text"); // sectionDetails.filter((value, index, self) => self.indexOf(value.text) === index);
-    controlDetailsDistinct =  this.removeDuplicates(controlDetails, "text"); // controlDetails.filter((value, index, self) => self.indexOf(value.text) === index);
-    sectionFactorDetailsDistinct = this.removeDuplicates(sectionFactorDetails, "text"); // sectionFactorDetails.filter((value, index, self) => self.indexOf(value.text) === index);
+    zoneDetailsDistinct = this.removeDuplicates(zoneDetails, "text");
+    sectionDetailsDistinct = this.removeDuplicates(sectionDetails, "text"); // sectionDetails.filter((value, index, self) => self.indexOf(value.text) === index);
+    controlDetailsDistinct = this.removeDuplicates(controlDetails, "text"); // controlDetails.filter((value, index, self) => self.indexOf(value.text) === index);
+    sectionFactorDetailsDistinct = this.removeDuplicates(
+      sectionFactorDetails,
+      "text"
+    ); // sectionFactorDetails.filter((value, index, self) => self.indexOf(value.text) === index);
     console.log("Distinct");
     console.log(sectionFactorDetailsDistinct);
     return (
@@ -106,14 +124,22 @@ export default class ManageWebpart extends React.Component<
             </div>
           </div>
           {items}
-          <div className={styles.row}>
+          <div
+            className={styles.row}
+            style={{
+              paddingLeft: "65px",
+              paddingTop: "20px"
+            }}
+          >
             <div>
               <div>
                 <Dropdown
                   label="Webparts"
-                  selectedKey={selectedWebpart ? selectedWebpart.key : undefined}
+                  selectedKey={
+                    selectedWebpart ? selectedWebpart.key : undefined
+                  }
                   onChange={this._onChangeWebpart}
-                  placeholder="Select an option"
+                  placeholder="Select a webpart"
                   options={[
                     {
                       key: "inUseWebparts",
@@ -138,10 +164,22 @@ export default class ManageWebpart extends React.Component<
               </div>
               <div>
                 <Dropdown
-                  label="Section"
-                  selectedKey={selectedSection ? selectedSection.key: undefined}
+                  label="Row"
+                  selectedKey={selectedZone ? selectedZone.key : undefined}
+                  onChange={this._onChangeZone}
+                  placeholder="Select a row"
+                  options={[...zoneDetailsDistinct]}
+                  styles={{ dropdown: { width: 300 } }}
+                />
+              </div>
+              <div>
+                <Dropdown
+                  label="Column"
+                  selectedKey={
+                    selectedSection ? selectedSection.key : undefined
+                  }
                   onChange={this._onChangeSection}
-                  placeholder="Select an option"
+                  placeholder="Select a column"
                   options={[...sectionDetailsDistinct]}
                   styles={{ dropdown: { width: 300 } }}
                 />
@@ -151,7 +189,7 @@ export default class ManageWebpart extends React.Component<
                   label="Order"
                   selectedKey={selectedOrder ? selectedOrder.key : undefined}
                   onChange={this._onChangeOrder}
-                  placeholder="Select an option"
+                  placeholder="Select an order"
                   options={[...controlDetailsDistinct]}
                   styles={{ dropdown: { width: 300 } }}
                 />
@@ -161,51 +199,131 @@ export default class ManageWebpart extends React.Component<
                   label="Column Factor"
                   selectedKey={selectedFactor ? selectedFactor.key : undefined}
                   onChange={this._onChangeFactor}
-                  placeholder="Select an option"
+                  placeholder="Select a layout"
                   options={[...sectionFactorDetailsDistinct]}
                   styles={{ dropdown: { width: 300 } }}
                 />
               </div>
+            </div>
+            <div
+              style={{
+                paddingTop: "20px",
+                paddingBottom: "20px"
+              }}
+            >
+              <DefaultButton
+                style={{
+                  paddingRight: "10px",
+                  marginRight: "10px",
+                  borderRadius: "5px",
+                  padding: "5px"
+                }}
+                text="Update"
+                onClick={this.updatePage.bind(this)}
+                allowDisabledFocus
+              />
+
+              <DefaultButton
+                style={{
+                  paddingRight: "10px",
+                  marginRight: "10px",
+                  borderRadius: "5px",
+                  padding: "5px"
+                }}
+                text="Cancel"
+                onClick={this.cancelPage.bind(this)}
+                allowDisabledFocus
+              />
+              {/* <button
+                onClick={this.updatePage.bind(this)}
+                style={{
+                  paddingRight: "10px",
+                  marginRight: "10px",
+                  borderRadius: "5px",
+                  padding: "5px"
+                }}
+              >
+                Update
+              </button>
+              <button
+                onClick={this.cancelPage}
+                style={{
+                  paddingRight: "10px",
+                  marginRight: "10px",
+                  borderRadius: "5px",
+                  padding: "5px"
+                }}
+              >
+                Cancel
+              </button> */}
             </div>
           </div>
         </div>
       </div>
     );
   }
+  private updatePage() {
+    console.log("=========Updated Page value start===========");
+    console.log(this.state);
+    console.log("=========Updated Page value end===========");
+  }
+
+  private cancelPage() {}
 
   private removeDuplicates(myArr, prop) {
-      return myArr.filter((obj, pos, arr) => {
-          return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
-      });
+    return myArr.filter((obj, pos, arr) => {
+      return arr.map(mapObj => mapObj[prop]).indexOf(obj[prop]) === pos;
+    });
   }
 
   private _onChangeWebpart = (evt, item) => {
-    if(item.text){
-      let selectedWebpart = this.props.webpart.filter(wp => wp.text === item.text)[0];
+    if (item.text) {
+      let selectedWebpart = this.props.webpart.filter(
+        wp => wp.text === item.text
+      )[0];
       this.setState({
         selectedWebpart: item,
-        selectedSection: {key: selectedWebpart.position.sectionIndex, text:selectedWebpart.position.sectionIndex},
-        selectedOrder: {key: selectedWebpart.position.controlIndex, text:selectedWebpart.position.controlIndex},
-        selectedFactor: {key: selectedWebpart.position.sectionFactor,text:selectedWebpart.position.sectionFactor}
+        selectedZone: {
+          key: selectedWebpart.position.zoneIndex,
+          text: selectedWebpart.position.zoneIndex
+        },
+        selectedSection: {
+          key: selectedWebpart.position.sectionIndex,
+          text: selectedWebpart.position.sectionIndex
+        },
+        selectedOrder: {
+          key: selectedWebpart.position.controlIndex,
+          text: selectedWebpart.position.controlIndex
+        },
+        selectedFactor: {
+          key: selectedWebpart.position.sectionFactor,
+          text: selectedWebpart.position.sectionFactor
+        }
       });
     }
   };
 
+  private _onChangeZone = (evt, item) => {
+    this.setState({
+      selectedZone: item
+    });
+  };
+
   private _onChangeSection = (evt, item) => {
     this.setState({
-      selectedSection: item.text,
+      selectedSection: item
     });
   };
 
   private _onChangeOrder = (evt, item) => {
     this.setState({
-      selectedOrder: item.text,
+      selectedOrder: item
     });
   };
 
   private _onChangeFactor = (evt, item) => {
     this.setState({
-      selectedFactor: item.text,
+      selectedFactor: item
     });
   };
 }
